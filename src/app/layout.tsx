c96bf092e9862500713ef6a1df542ff0e549b82b
Tina Huynh <tinamchuynh@gmail.com>
2025-06-18 23:32:44 -0700

    feat: add global styles and layout component for Next.js application

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import Navigation from "@/components/Navigation";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Intro to Web - MDX Demo",
+  description:
+    "A Next.js project with MDX support for web development learning",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navigation />
+        <main className="mx-auto px-4 py-8 container">{children}</main>
+      </body>
+    </html>
+  );
+}

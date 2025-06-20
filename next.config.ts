7a0938a9d1eb0ab459fb8f9165e79da0d14c103f
Tina Huynh <tinamchuynh@gmail.com>
2025-06-19 18:19:47 -0700

    feat: enhance MDX configuration by adding rehype-autolink-headings plugin for improved heading links

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 import remarkToc from "remark-toc";
@@ -16,7 +17,18 @@ const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
     remarkPlugins: [remarkGfm, remarkToc],
-    rehypePlugins: [rehypeSlug],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["anchor"],
+          },
+        },
+      ],
+    ],
   },
 });
 

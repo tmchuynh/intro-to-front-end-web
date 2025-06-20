443990aebaba559045fa733e21be3da42cb7f88c
Tina Huynh <tinamchuynh@gmail.com>
2025-06-19 18:15:40 -0700

    feat: enhance MDX configuration with additional remark and rehype plugins for improved markdown processing

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
+import remarkToc from "remark-toc";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
@@ -10,10 +12,11 @@ const nextConfig: NextConfig = {
 };
 
 const withMDX = createMDX({
+  extension: /\.mdx?$/,
   // Add markdown plugins here, as desired
   options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
+    remarkPlugins: [remarkGfm, remarkToc],
+    rehypePlugins: [rehypeSlug],
   },
 });
 
